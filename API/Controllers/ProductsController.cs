7b814f1d20dcf86b4d6418458c62d13243f1e6e2
heber mattos <113201577+hmattosaffinitiv@users.noreply.github.com>
2024-01-29 10:05:27 -0300

    products

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,4 +1,7 @@
+using System.Drawing;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Products.Controllers;
 
@@ -14,15 +17,32 @@ public class ProductsController : ControllerBase
     }
 
     [HttpGet]
-    public IEnumerable<Product> Get()
+    public IEnumerable<Product> Get(int page = 1, int size = 10)
     {
-        return _context.Products;
+        return _context.Products.OrderBy(x => x.Id)
+                        .Skip((page - 1) * size)
+                        .Take(size);
     }
 
     [HttpPost]
     public async Task Post(Product model)
     {
         await _context.Products.AddAsync(model);
+
+        await _context.SaveChangesAsync();
+    }
+
+    [HttpPut("{id}")]
+    public async Task Put(int id, Product model)
+    {
+        var product = await _context.Products.FirstOrDefaultAsync(x => x.Id == id);
+
+        if (product == null)
+            throw new ArgumentException("product not found");
+
+        product.Name = model.Name;
+        product.Description = model.Description;
+        product.Price = model.Price;
 
         await _context.SaveChangesAsync();
     }
